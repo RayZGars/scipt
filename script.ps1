ecd85e62064c0bacb87a613d91aa1e9720d133e7
RayZGars <146269023+RayZGars@users.noreply.github.com>
2023-10-26 20:17:08 +1300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,9 +2,10 @@ write-Host "Starting"
 
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
+    Write-Output "You must run powershell -File C:/Users/yourusername/script.ps1 as an admin opening windows powershell as a admin"
     Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://raw.githubusercontent.com/RayZGars/scipt/main/script.ps1 | iex"
-    break
+
+   break
 }
 
 
@@ -17,5 +18,4 @@ write-Host "Setting high peformance"
 powercfg.exe -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
 
-write-Host "Worked"
-
+Read-Host -Prompt "Press Enter to exit"
