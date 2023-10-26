e63b4d349dea1ada0f39700fd7ec64024889cff9
RayZGars <146269023+RayZGars@users.noreply.github.com>
2023-10-26 19:52:44 +1300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,13 @@
 write-Host "Starting"
 
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://raw.githubusercontent.com/RayZGars/scipt/main/script.ps1 | iex"
+    break
+}
+
+
 winget install -e --id Mozilla.Firefox
 
 write-Host "Done installing "
@@ -7,7 +15,6 @@ write-Host "Done installing "
 write-Host "Setting high peformance"
 
 powercfg.exe -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-
 
 
 write-Host "Worked"
