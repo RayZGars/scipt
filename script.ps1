107c6ca352408f582c8ff0760035c52135c94dfb
RayZGars <146269023+RayZGars@users.noreply.github.com>
2023-10-26 21:08:28 +1300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -20,28 +20,6 @@ powercfg.exe -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
 write-Host "Debloating removing pre-installed apps"
 
-Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage
-Get-AppxPackage *AV1VideoExtension* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.549981C3F5F10* | Remove-AppxPackage
-Get-AppxPackage *WindowsFeedbackHub* | Remove-AppxPackage
-Get-AppxPackage *HEIFImageExtension* | Remove-AppxPackage
-Get-AppxPackage *GetHelp* | Remove-AppxPackage
-Get-AppxPackage *WindowsMaps* | Remove-AppxPackage
-Get-AppxPackage *Todos* | Remove-AppxPackage
-Get-AppxPackage *ZuneVideo* | Remove-AppxPackage
-Get-AppxPackage *MicrosoftOfficeHub* | Remove-AppxPackage
-Get-AppxPackage *BingNews* | Remove-AppxPackage
-Get-AppxPackage *OneDriveSync* | Remove-AppxPackage
-Get-AppxPackage *Windows.Photos* | Remove-AppxPackage
-Get-AppxPackage *SkypeApp* | Remove-AppxPackage
-Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
-Get-AppxPackage *SpotifyAB.SpotifyMusic* | Remove-AppxPackage
-Get-AppxPackage *Teams* | Remove-AppxPackage
-Get-AppxPackage *WindowsSoundRecorder* | Remove-AppxPackage
-Get-AppxPackage *BingWeather* | Remove-AppxPackage
-Get-AppxPackage *YourPhone* | Remove-AppxPackage
-
-
 
 Function StartMenuTiles {
 $key = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\*start.tilegrid`$windows.data.curatedtilecollection.tilecollection\Current"
