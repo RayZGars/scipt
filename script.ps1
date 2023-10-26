c030c0964924c95886a7f701420fa6f9f1919877
RayZGars <146269023+RayZGars@users.noreply.github.com>
2023-10-27 10:10:15 +1300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -16,6 +16,52 @@ powercfg.exe -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
 write-Host "Removing bloatware" 
 
+# Packages
+
+Get-AppxPackage -name “Microsoft.ZuneMusic” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Music.Preview” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.XboxIdentityProvider” | Remove-AppxPackage
+Get-AppxPackage *Clipchamp* | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingTravel” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.BingHealthAndFitness” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.BingFoodAndDrink” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.People” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.BingFinance” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.3DBuilder” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.WindowsCalculator” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.BingNews” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.XboxApp” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.BingSports” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.WindowsCamera” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.Getstarted” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.Office.OneNote” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.WindowsMaps” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.MicrosoftSolitaireCollection” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.MicrosoftOfficeHub” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.BingWeather” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.BioEnrollment” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.Windows.Photos” | Remove-AppxPackage
+
+Get-AppxPackage -name “Microsoft.WindowsPhone” | Remove-AppxPackage
 
 
 
